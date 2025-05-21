63f811772495cad09e487915876e679a880edb1d
Daniel Ulshen <dulshen@gmail.com>
2025-05-21 22:31:26 +0000

    refactoring climenus
    to encapsulate more of the UI functionality
    
    and updating addRecipe based on these changes

--- climenus/climenus_test.go
+++ climenus/climenus_test.go
@@ -6,21 +6,21 @@ import (
 
 var dummyMenus = []struct {
 	description string
-	menuKey     string
+	name        string
 }{
-	{description: "dummy1", menuKey: "dummy1"},
-	{description: "dummy2", menuKey: "dummy2"},
-	{description: "dummy3", menuKey: "dummy3"},
+	{description: "dummy1", name: "dummy1"},
+	{description: "dummy2", name: "dummy2"},
+	{description: "dummy3", name: "dummy3"},
 }
 
-func TestBuildMenuDescription(t *testing.T) {
+func TestMenuDescriptions(t *testing.T) {
 	testCases := []struct {
 		name         string
 		description  string
 		expected     string
 		dummyOptions []struct {
 			description string
-			menuKey     string
+			name        string
 		}
 	}{
 		{
@@ -45,37 +45,38 @@ func TestBuildMenuDescription(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var menuOptions []map[string]string
+			var menu Menu
 
 			for _, option := range tc.dummyOptions {
-				menuOptions = append(menuOptions, map[string]string{"description": option.description, "menuKey": option.menuKey})
+				c := Command{
+					Name:        option.name,
+					Description: option.description,
+				}
+				menu.AddCommand(&c)
 			}
 
-			menuOptions = append(menuOptions, map[string]string{"description": tc.description, "menuKey": "key1"})
-
-			results, err := BuildMenu(menuOptions)
+			c := Command{
+				Name:        "name1",
+				Description: tc.description,
+			}
+			menu.AddCommand(&c)
 
 			expectedPosition := len(tc.dummyOptions)
 
-			if results[expectedPosition].Description != tc.expected {
-				t.Errorf(`Expected %v, got %v`, tc.expected, results[expectedPosition].Description)
+			if menu.Commands[expectedPosition].Description != tc.expected {
+				t.Errorf(`Expected %v, got %v`, tc.expected, menu.Commands[expectedPosition].Description)
 			}
-
-			if err != nil {
-				t.Errorf(`got error: %v`, err)
-			}
-
 		})
 	}
 }
 
-func TestBuildMenuNumbers(t *testing.T) {
+func TestMenuOptionNumbers(t *testing.T) {
 	testCases := []struct {
 		name         string
 		expected     int
 		dummyOptions []struct {
 			description string
-			menuKey     string
+			name        string
 		}
 	}{
 		{
@@ -92,76 +93,164 @@ func TestBuildMenuNumbers(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var menuOptions []map[string]string
+			var menu Menu
 
 			for _, option := range tc.dummyOptions {
-				menuOptions = append(menuOptions, map[string]string{"description": option.description, "menuKey": option.menuKey})
+				menu.AddCommand(&(Command{
+					Name:        option.name,
+					Description: option.description,
+				}))
 			}
 
-			menuOptions = append(menuOptions, map[string]string{"description": "descrip1", "menuKey": "key1"})
-
-			results, err := BuildMenu(menuOptions)
-
+			menu.AddCommand(&(Command{
+				Name:        "name1",
+				Description: "descrip1",
+			}))
 			expectedPosition := len(tc.dummyOptions)
 
-			if results[expectedPosition].OptionNumber != tc.expected {
-				t.Errorf(`Expected %v, got %v`, tc.expected, results[expectedPosition].OptionNumber)
+			if menu.Commands[expectedPosition].OptionNumber != tc.expected {
+				t.Errorf(`Expected %v, got %v`, tc.expected, menu.Commands[expectedPosition].OptionNumber)
 			}
-
-			if err != nil {
-				t.Errorf(`got error: %v`, err)
-			}
-
 		})
 	}
 }
 
-func TestBuildMenuKeys(t *testing.T) {
+func TestMenuNames(t *testing.T) {
 	testCases := []struct {
 		name         string
-		menuKey      string
+		optionName   string
 		expected     string
 		dummyOptions []struct {
 			description string
-			menuKey     string
+			name        string
 		}
 	}{
 		{
-			name:         "test_menukey_1",
-			menuKey:      "key1",
-			expected:     "key1",
+			name:         "test_name_1",
+			optionName:   "name1",
+			expected:     "name1",
 			dummyOptions: nil,
 		},
 		{
-			name:         "test_menukey_2",
-			menuKey:      "key2",
-			expected:     "key2",
+			name:         "test_name_2",
+			optionName:   "name2",
+			expected:     "name2",
 			dummyOptions: dummyMenus,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			var menuOptions []map[string]string
+			var menu Menu
 
 			for _, option := range tc.dummyOptions {
-				menuOptions = append(menuOptions, map[string]string{"description": option.description, "menuKey": option.menuKey})
+				menu.AddCommand(&(Command{
+					Name:        option.name,
+					Description: option.description,
+				}))
 			}
 
-			menuOptions = append(menuOptions, map[string]string{"description": "descrip", "menuKey": tc.menuKey})
-
-			results, err := BuildMenu(menuOptions)
+			menu.AddCommand(&(Command{
+				Name:        tc.optionName,
+				Description: "descrip",
+			}))
 
 			expectedPosition := len(tc.dummyOptions)
 
-			if results[expectedPosition].MenuKey != tc.expected {
-				t.Errorf(`Expected %v, got %v`, tc.expected, results[expectedPosition].MenuKey)
+			if menu.Commands[expectedPosition].Name != tc.expected {
+				t.Errorf(`Expected %v, got %v`, tc.expected, menu.Commands[expectedPosition].Name)
 			}
+		})
+	}
+}
+
+func TestCommandByNumber(t *testing.T) {
+	testCases := []struct {
+		name             string
+		numberOfCommands int
+		expected         string
+		dummyOptions     []struct {
+			description string
+			name        string
+		}
+	}{
+		{
+			name:             "testOption1",
+			numberOfCommands: 1,
+			expected:         dummyMenus[0].name,
+			dummyOptions:     dummyMenus[:1],
+		},
+		{
+			name:             "testOption2",
+			numberOfCommands: 2,
+			expected:         dummyMenus[1].name,
+			dummyOptions:     dummyMenus[:2],
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var menu Menu
+
+			for _, option := range tc.dummyOptions {
+				menu.AddCommand(&Command{Name: option.name, Description: option.description})
+			}
+
+			c, err := menu.CommandByOptionNumber(tc.numberOfCommands)
 
 			if err != nil {
-				t.Errorf(`got error: %v`, err)
+				t.Errorf("got error %s", err.Error())
 			}
 
+			if c.Name != tc.expected {
+				t.Errorf("expected %v, got %v", tc.expected, c.Name)
+			}
+		})
+	}
+}
+
+func TestGetCommand(t *testing.T) {
+	testCases := []struct {
+		name         string
+		commandName  string
+		expected     string
+		dummyOptions []struct {
+			description string
+			name        string
+		}
+	}{
+		{
+			name:         "testGetCommand1",
+			commandName:  "TestCommand1",
+			expected:     "TestCommand1",
+			dummyOptions: nil,
+		},
+		{
+			name:         "testGetCommand2",
+			commandName:  "TestCommand2",
+			expected:     "TestCommand2",
+			dummyOptions: dummyMenus,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var menu Menu
+			for _, option := range tc.dummyOptions {
+				menu.AddCommand(&Command{Name: option.name, Description: option.description})
+			}
+
+			menu.AddCommand(&Command{Name: tc.commandName, Description: "testDescription"})
+
+			c, err := menu.Command(tc.commandName)
+
+			if err != nil {
+				t.Errorf("got error %v", err.Error())
+			}
+
+			if c.Name != tc.expected {
+				t.Errorf("got %v, expected %v", c.Name, tc.expected)
+			}
 		})
 	}
 }
